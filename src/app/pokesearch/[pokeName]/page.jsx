c5cb36ea86afac527bfc6d80fb11ef16e2401b9a
Whale-B <sarawut.tesla@gmail.com>
2024-05-30 23:14:16 +0700

    first commit

--- src/app/pokesearch/[pokeName]/page.jsx
+++ src/app/pokesearch/[pokeName]/page.jsx
@@ -22,6 +22,7 @@ function PokeResult() {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokeName}`);
       const data = await response.json();
       setPokeData(data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -42,7 +43,6 @@ function PokeResult() {
                     <>
                         <h3 className='text-3xl'>{pokeData.name}</h3>
                         <Image src={pokeData.sprites?.other.dream_world.front_default} width={300} height={300} alt={pokeData.name}/>
-                        <Image src={pokeData.sprites?.other['official-artwork'].front_default} width={300} height={300} alt={pokeData.name}/>
                         <div className='mt-5'>
                             <p className='my-3'>Weight:{pokeData.weight}</p>
                             <p className='my-3'>
